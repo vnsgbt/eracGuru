c3750f00fe02d44ea56f7d390cb4cb9e71eaa4ff
steve n <vnsgbt@gmail.com>
2016-12-13 19:39:02 -0600

    component template, ngModel binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{guru.name}}</h2>
+    <div>
+      <label>id: </label>
+      {{guru.id}}
+    </div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="guru.name" placeholder="name">
+    </div>
+  `,
 })
-export class AppComponent  { name = 'Gurus'; }
+
+export class AppComponent  {
+  title = 'Erac Gurus' 
+  name = 'Comps QAs' 
+  guru: Guru = {
+    id: 1,
+    name: 'Ravi'
+  }
+}
+
+export class Guru {
+  id: number
+  name: string
+}
