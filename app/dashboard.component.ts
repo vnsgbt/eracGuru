2d9b904c284d5e26b69e6f395f36417f836c8770
steve n <vnsgbt@gmail.com>
2016-12-13 22:21:13 -0600

    module-relative loading

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+
+import { Guru } from './guru'
+import { GuruService } from './guru.service' 
 
 @Component({
+  moduleId: module.id,
   selector: 'my-dashboard',
-  template: '<h3>My Dashboard</h3>'
+  templateUrl: 'partial/dashboard.component.html',
 })
 
-export class DashboardComponent { }
+export class DashboardComponent implements OnInit {
+    
+    gurus: Guru[] = []
+
+    constructor(private guruService: GuruService){}
+    
+    ngOnInit() {
+        this.guruService.getGurus()
+            .then(gurus => this.gurus = gurus.slice(1,5))
+    }
+}
