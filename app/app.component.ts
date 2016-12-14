e48ca10407dd3f991ba362451d29c06e57cf3d15
steve n <vnsgbt@gmail.com>
2016-12-13 20:26:31 -0600

    *ng directives, event binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,24 +4,31 @@ import { Component } from '@angular/core'
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <h2>{{guru.name}}</h2>
-    <div>
-      <label>id: </label>
-      {{guru.id}}
-    </div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="guru.name" placeholder="name">
+    <h2>My Gurus</h2>
+    <ul>
+      <li *ngFor="let guru of gurus"
+        (click)="onSelect(guru)">
+        <span>{{guru.id}}</span> {{guru.name}}
+      </li>
+    </ul>
+    <div *ngIf="selectedGuru">
+      <h2>{{selectedGuru.name}} details!</h2>
+      <div><label>id: </label>{{selectedGuru.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedGuru.name" placeholder="name"/>
+      </div>
     </div>
   `,
 })
 
 export class AppComponent  {
   title = 'Erac Gurus' 
-  name = 'Comps QAs' 
-  guru: Guru = {
-    id: 1,
-    name: 'Ravi'
+  gurus = GURUS
+  selectedGuru: Guru
+
+  onSelect(guru: Guru) {
+    this.selectedGuru = guru
   }
 }
 
@@ -29,3 +36,16 @@ export class Guru {
   id: number
   name: string
 }
+
+let GURUS: Guru[] = [
+  { id: 11, name: 'Wendy'},
+  { id: 12, name: 'Adam'},
+  { id: 13, name: 'Anantha'},
+  { id: 13, name: 'Shaul'},
+  { id: 13, name: 'Jon'},
+  { id: 14, name: 'Dan'},
+  { id: 15, name: 'Rich'},
+  { id: 16, name: 'Harita'},
+  { id: 17, name: 'Tim'},
+  { id: 18, name: 'Steve'},
+]
